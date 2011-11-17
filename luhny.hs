b097c34a5cb15d3c17bee8e4556e68abbad7c9bc
Stefan Zeiger <szeiger@novocode.com>
2011-11-17 22:48:21 +0100

    Simplify + remove GHC dependency

--- luhny.hs
+++ luhny.hs
@@ -1,28 +1,24 @@
 import Data.List
 import Data.Char
 import Data.Maybe
-import GHC.IO.Handle
-import GHC.IO.Handle.FD
+import System.IO
 
 luhn cs = lsum cs `mod` 10 == 0
   where lsum = sum . zipWith f [1..] . map digitToInt . reverse
-        f i x = let y = (2 - mod i 2) * x in if y > 9 then y - 9 else y
+        f i x = uncurry (+) $ ((2-i `mod` 2) * x) `divMod` 10
 
-luhnStart cs =
-  if length ds < 14 then 0
-  else fromMaybe 0 $ find (>0) $ map (dluhn ds) [16,15,14]
-  where ds = filter isDigit cs
-        dluhn cs len = let cs2 = take len cs in
-          if length cs2 < len then 0 else if luhn cs2 then len else 0
-
-mapGroups cs = concatMap (tr 0) $ groupBy (\a b -> isD a == isD b) cs
-  where isD = flip elem "0123456789- "
+luhnStart cs = fromMaybe 0 $ find (>0) $ map (dluhn cs) [16,15,14]
+  where dluhn cs len = let es = take len cs in
+          if length es >= len && luhn es then len else 0
 
 tr seq [] = []
 tr seq (x:xs) =
-  let m = max seq $ luhnStart $ x:xs in
+  let m = max seq $ luhnStart $ filter isDigit $ x:xs in
     if isDigit x then (if m > 0 then 'X' else x) : tr (m-1) xs
     else x : tr seq xs
 
-main = hSetBinaryMode stdin True >> hSetBinaryMode stdout True >>
+mapGroups cs = groupBy (\a b -> isD a == isD b) cs >>= tr 0
+  where isD c = elem c "0123456789- "
+
+main = mapM (flip hSetBinaryMode True) [stdin, stdout] >>
   getContents >>= putStr . mapGroups
