8dbf8351e70f50624a78b8acc6e8c63ad88501a6
Stefan Zeiger <szeiger@novocode.com>
2011-11-17 23:31:12 +0100

    simplify

--- luhny.hs
+++ luhny.hs
@@ -1,21 +1,19 @@
 import Data.List
 import Data.Char
-import Data.Maybe
 import System.IO
 
 luhn cs = lsum cs `mod` 10 == 0
   where lsum = sum . zipWith f [1..] . map digitToInt . reverse
         f i x = uncurry (+) $ ((2-i `mod` 2) * x) `divMod` 10
 
-luhnStart cs = fromMaybe 0 $ find (>0) $ map (dluhn cs) [16,15,14]
+luhnLen cs = maximum $ map (dluhn $ filter isDigit cs) [14..16]
   where dluhn cs len = let es = take len cs in
           if length es >= len && luhn es then len else 0
 
-tr seq [] = []
-tr seq (x:xs) =
-  let m = max seq $ luhnStart $ filter isDigit $ x:xs in
-    if isDigit x then (if m > 0 then 'X' else x) : tr (m-1) xs
-    else x : tr seq xs
+tr i [] = []
+tr i (x:xs) = let m = max i $ luhnLen (x:xs) in
+  if isDigit x then (if m > 0 then 'X' else x) : tr (m-1) xs
+  else x : tr i xs
 
 mapGroups cs = groupBy (\a b -> isD a == isD b) cs >>= tr 0
   where isD c = elem c "0123456789- "
